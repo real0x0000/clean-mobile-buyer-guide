3576b6f7d02479fa91c62eeefe51e0fa5d2cb086
Anuwat Sittichak <anuwat.sit@gmail.com>
2018-09-20 10:55:39 +0700

    Init xcode project.

---
+++ Clean Mobile Buyer GuideUITests/Clean_Mobile_Buyer_GuideUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Clean_Mobile_Buyer_GuideUITests.swift
+//  Clean Mobile Buyer GuideUITests
+//
+//  Created by ANUWAT SITTICHAK on 20/9/2561 BE.
+//  Copyright © 2561 ANUWAT SITTICHAK. All rights reserved.
+//
+
+import XCTest
+
+class Clean_Mobile_Buyer_GuideUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
