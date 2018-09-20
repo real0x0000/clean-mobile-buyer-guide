ba772cb05ef1f56d6c03f2ce71b0783363a41be0
Anuwat Sittichak <anuwat.sit@gmail.com>
2018-09-20 12:26:18 +0700

    Add MobileCell.

---
+++ Clean Mobile Buyer Guide/MobileList/MobileListPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MobileListPresenter.swift
+//  Clean Mobile Buyer Guide
+//
+//  Created by ANUWAT SITTICHAK on 20/9/2561 BE.
+//  Copyright © 2561 ANUWAT SITTICHAK. All rights reserved.
+//
+
+import Foundation
+
+class MobileListPresenter {
+    
+}
